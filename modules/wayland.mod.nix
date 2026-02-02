a2ea649cf69b404b4b4f005e50d9b1288b49680e
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2026-02-03 09:18:03 +1000

    fix: mkIf niri

--- modules/wayland.mod.nix
+++ modules/wayland.mod.nix
@@ -6,26 +6,28 @@
     ...
   }: let
     inherit (lib) mkIf;
-  in {
-    home.packages = [
-      pkgs.wl-clipboard
-      pkgs.xdg-utils
-    ];
+  in
+    {
+      home.packages = [
+        pkgs.wl-clipboard
+        pkgs.xdg-utils
+      ];
 
-    programs.niri.settings.environment = mkIf config.programs.niri.enable {
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
-      XDG_SESSION_TYPE = "wayland";
-      MOZ_ENABLE_WAYLAND = "1";
-      SDL_VIDEODRIVER = "wayland,x11";
-    };
-
-    wayland.windowManager.hyprland.settings.env = [
-      "ELECTRON_OZONE_PLATFORM_HINT,auto"
-      "MOZ_ENABLE_WAYLAND,1"
-      "SDL_VIDEODRIVER,wayland,x11"
-      "XDG_SESSION_TYPE,wayland"
-    ];
-  };
+      wayland.windowManager.hyprland.settings.env = [
+        "ELECTRON_OZONE_PLATFORM_HINT,auto"
+        "MOZ_ENABLE_WAYLAND,1"
+        "SDL_VIDEODRIVER,wayland,x11"
+        "XDG_SESSION_TYPE,wayland"
+      ];
+    }
+    // (mkIf config.programs.niri.enable {
+      programs.niri.settings.environment = {
+        ELECTRON_OZONE_PLATFORM_HINT = "auto";
+        XDG_SESSION_TYPE = "wayland";
+        MOZ_ENABLE_WAYLAND = "1";
+        SDL_VIDEODRIVER = "wayland,x11";
+      };
+    });
 
   flake.nixosModules.wayland = {
     hardware.graphics.enable = true;
