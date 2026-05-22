cf96079f0f2129983cb26d2312189cabd2050e0b
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2026-05-22 15:15:37 +1000

    chore(gtk): force gtk theme

--- modules/gtk.mod.nix
+++ modules/gtk.mod.nix
@@ -1,7 +1,13 @@
 {
-  flake.homeModules.gtk = {config, ...}: {
+  flake.homeModules.gtk = {
+    config,
+    lib,
+    ...
+  }: let
+    inherit (lib) mkForce;
+  in {
     gtk.enable = true;
-    gtk.gtk4.theme = config.gtk.theme;
+    gtk.gtk4.theme = mkForce config.gtk.theme;
   };
 
   flake.nixosModules.gtk = {
